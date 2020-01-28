87649ed1b2a312bea80f596a4a512ec5f346ebfa
Charlie Tung <choelestung@gmail.com>
2020-01-28 13:24:56 -0500

    added register/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Main from "./containers/Main";
-import ShoppingCart from "./containers/ShoppingCart";
+// import Main from "./containers/Main";
+// import ShoppingCart from "./containers/ShoppingCart";
 import "./styles/App.css";
-import { Navbar, Nav, Button } from "react-bootstrap";
-import PetPage from "./containers/PetPage";
+import { Navbar, Nav, Button, Row } from "react-bootstrap";
+// import PetPage from "./containers/PetPage";
 import Home from "./containers/Home"
 import Footer from "./containers/Footer"
 import { connect } from "react-redux"
@@ -17,6 +17,8 @@ import {
 import { bindActionCreators } from 'redux'
 import fetchYelp from './config/apis/fetchYelp'
 import UserLocationForm from "./components/UserLocationForm";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 
 
@@ -30,13 +32,17 @@ class App extends Component {
 
     this.shouldComponentRender = this.shouldComponentRender.bind(this);
   }
-
+  // ====================================
+  // Life Cycle methods
   componentDidMount() {
     // const { fetchYelp } = this.props;
     this.geoLocate();
 
     // When do I fire off this function???
   }
+
+  // ====================================
+  // Class methods
 
   shouldComponentRender() {
     // const { pending } = this.props;
@@ -70,17 +76,20 @@ class App extends Component {
     let input = e.target.value;
     this.setState({
       value: input
-    })
+    });
 
     // console.log("updating location")
   };
   submit = e => {
     e.preventDefault();
     // Updates the location with new location
+    // Google needs to know about location for the MAP
+    // Yelp needs to know the location for animal shelters
     console.log("submits");
   };
 
   render() {
+
     // const { user } = this.props
     return (
       <Router>
@@ -90,18 +99,26 @@ class App extends Component {
               Trade Pals
             </Link>
 
-            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Link className="index-pet" to="/index/pets">
+              {/* <Link className="index-pet" to="/index/pets">
                 All Pets
-              </Link>
+              </Link> */}
               <Nav className="mr-auto"></Nav>
-              <Link className="cart" to="/shoppingcart">
-                <Button className="mr-sm-2" variant="outline-success">
-                  Cart
-                </Button>
-              </Link>
-            </Navbar.Collapse> */}
+              <Row className="authentication-link">
+                <Link to="/login">
+                  <Login />
+                </Link>
+                <Link to="/register">
+                  <Register />
+                </Link>
+                <Link className="cart" to="/shoppingcart">
+                  <Button className="mr-sm-2" variant="outline-success">
+                    Cart
+                  </Button>
+                </Link>
+              </Row>
+            </Navbar.Collapse>
           </Navbar>
         </header>
 
@@ -115,9 +132,16 @@ class App extends Component {
           <Route path="/petpage">
             <PetPage />
           </Route> */}
+
+          <Route path="/login">Login Form</Route>
+          <Route path="/register">sign up form</Route>
           <Route path="/">
             <div className="m-0 contento">
-              <UserLocationForm value={this.state.value} updateLocation={this.updateLocation} submit={this.submit} />
+              <UserLocationForm
+                value={this.state.value}
+                updateLocation={this.updateLocation}
+                submit={this.submit}
+              />
               <Home />
               <Footer />
             </div>
