5fbfe076737467b059e91ceac8dd3eb1dd71e50a
Charlie Tung <choelestung@gmail.com>
2020-01-29 12:13:33 -0500

    register form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,20 +3,45 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap"
 
 const registerForm = {
   border: "solid 1px black",
+  maxWidth: "500px",
+  minWidth: "50vw",
+  // width: "",
+  height: "auto"
 };
 
 const registerCon = {
   border: "solid 1px black",
-  color: "white",
+  // color: "white",
+  justifyContent: "flex-end",
+  display: "flex",
+  alignItems: "center",
 }
+
+
+// on small screens center not flex-end
 const Register = () => {
   return (
     <Container fluid style={registerCon}>
       <Row>
-        <Col>
-          <div>hello</div>
+        <Col sm={12}>
           <form style={registerForm} >
             <div className="form-group">
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
+              <label htmlFor="name">name</label>
+              <input type="text" className="form-control" placeholder="Enter Your Name"/>
               <label htmlFor="name">name</label>
               <input type="text" className="form-control" placeholder="Enter Your Name"/>
             </div>
--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Footer = (props) => {
-  // console.log(this.props.location.pathname);
   const { absFoot } = props
   return (
     <footer className={`justify-content-center ${absFoot} footer`} >
